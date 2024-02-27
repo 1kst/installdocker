d3a2bbfde68195920ff083c0b753d75b9b1f5911
1kst <fxcktg@gmail.com>
2024-02-27 16:43:16 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 安装docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+# 检查docker是否安装成功
+if ! command -v docker &> /dev/null
+then
+    echo "Docker安装失败"
+    exit 1
+fi
+
+# 安装docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# 检查docker-compose是否安装成功
+if ! command -v docker-compose &> /dev/null
+then
+    echo "Docker-compose安装失败"
+    exit 1
+fi
+
+# 给权限
+chmod +x /usr/local/bin/docker-compose
+usermod -aG docker $USER
+
+# 输出安装成功信息
+echo "Docker和Docker-compose安装成功"
